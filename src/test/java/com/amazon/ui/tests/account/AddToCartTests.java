6d197728c87d364e189bdc5a73fc9265ee3c1793
Aslanov Nijad <aslanlinicad@gmail.com>
2023-07-11 16:27:20 -0700

    added comments

--- src/test/java/com/amazon/ui/tests/account/AddToCartTests.java
+++ src/test/java/com/amazon/ui/tests/account/AddToCartTests.java
@@ -13,32 +13,44 @@ import static com.amazon.ui.constants.TestConstants.CART_EMPTY_MSG;
 @Listeners(TestListener.class)
 public class AddToCartTests extends BaseAddToCart {
 
-    @Test(groups = {"p1", "account"})
+    @Test(groups = {"p1", "account"}, description = "This test validates that a user can add an item to cart and delete it.")
     void can_add_item_to_cart_and_delete() {
 
+        // Searching for the item
         String selectedProductTitle = accountPage.get()
                 .fillSearchBar("headphone")
                 .pressSearch()
                 .clickFirstProductLink()
                 .getSelectedProductTitle();
 
+        // Adding the item to the cart
         accountPage.get()
                 .selectRadioButton()
                 .addToCart()
                 .dismissAddOns();
 
+        // Checking if the item is added to the cart
         Assert.assertEquals(accountPage.get().getConfirmationMessage(), ADDED_TO_CART_MSG, "No Confirm Msg");
 
+        // Navigate to the shopping cart
         accountPage.get().goToCart();
 
+        // Fetch the title of the item in the shopping cart
         String shoppingCartItemName = accountPage.get().getElementText(AccountPage.LOCATOR_CART_ITEM);
+
+        // Assert that the product title matches the selected product's title
         Assert.assertEquals(shoppingCartItemName, selectedProductTitle, "Product title not matching");
 
+        // Remove the item from the cart
         accountPage.get().removeItemFromCart();
 
+        // Fetch the message displayed when the cart is empty
         String actualMsg = accountPage.get().getElementText(AccountPage.LOCATOR_EMPTY_CART_MSG).trim();
+
+        // Assert that the message displayed matches the expected "Cart Empty" message
         Assert.assertEquals(actualMsg, CART_EMPTY_MSG, "Empty cart message mismatch");
 
     }
 
 }
+
--- src/test/java/com/amazon/ui/tests/login/LoginTests.java
+++ src/test/java/com/amazon/ui/tests/login/LoginTests.java
@@ -1,7 +1,6 @@
 package com.amazon.ui.tests.login;
 
 import com.amazon.ui.listeners.TestListener;
-import jdk.jfr.Description;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -11,22 +10,29 @@ import static com.amazon.ui.constants.TestConstants.SIGN_IN_TITLE;
 @Listeners(TestListener.class)
 public class LoginTests extends BaseLoginTest {
 
-    @Test(groups = {"p1", "login"})
-    @Description("Login with valid credentials")
+    @Test(groups = {"p1", "login"}, description = "Tests whether user can login with valid credentials")
     void can_login_with_good_credentials() {
 
-        loginPage.get()
-                .hoverAccountList()
+        // Hover over the account list on the login page
+        loginPage.get().hoverAccountList()
+                // Click on the sign in button
                 .clickSignInButton()
+                // Enter the user email
                 .enterEmail(environmentConfiguration.email())
+                // Click continue to proceed with the login process
                 .clickContinueButton()
+                // Enter the user password
                 .enterPassword(environmentConfiguration.password())
+                // Click on the sign-in button to submit the credentials
                 .clickSignInWelcomeBackButton();
+
+        // Wait until the page title is SIGN_IN_TITLE
         accountPage.get().waitForTitle(SIGN_IN_TITLE);
+
+        // Assert that the actual title of the page matches the expected SIGN_IN_TITLE
         Assert.assertEquals(accountPage.get().getActualTitle(), SIGN_IN_TITLE, "Page title does not match expected");
 
     }
-
 }
 
 

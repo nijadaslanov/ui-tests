ea8dffb36770e4dfaecaef7fe077ae9dbaf0069b
Aslanov Nijad <aslanlinicad@gmail.com>
2023-07-12 10:31:50 -0700

    removed packages

--- .scripts/awsCredentials.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-aws sso login --profile automation-api-test
-clear
-read -p "Press enter to continue the process"
-export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domain viacustomers --domain-owner 214454900609 --query authorizationToken --output text --profile automation-api-test --region us-west-2`
-echo $CODEARTIFACT_AUTH_TOKEN
